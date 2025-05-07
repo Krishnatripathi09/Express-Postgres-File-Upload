aceaa0a0eebca9d71aee1f60c96167997707d56b
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-05-07 16:45:33 +0530

    feat(timestamp: Addded the timestamps on user Schema torecord timestamp for users

--- models/user.js
+++ models/user.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    maxLength: 50,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    minLength: 5,
-    maxLength: 30,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 5,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
