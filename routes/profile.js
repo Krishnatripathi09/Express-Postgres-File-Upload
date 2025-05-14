3715cfba6e6c5fd0798a078dd7c9741d9798afc0
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-05-14 21:02:23 +0530

    feat(signup): Made the Sign-Up API Dynamic to receive data from User

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,9 @@
 const express = require("express");
-
+const User = require("../models/user.js");
 const authRouter = express.Router();
 
-authRouter.post("/signup", (req, res) => {});
+authRouter.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+});
