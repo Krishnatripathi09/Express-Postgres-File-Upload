b5b85a29c1c96debae3558b5401565d0cb530583
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-05-16 16:42:25 +0530

    feat(password): Installed bcrypt to encrypt user Password

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
+  const passwordHash = await bcrypt.hash(password, 10);
+
   const userData = await User({
     firstName,
     lastName,
     email,
-    password,
+    password: passwordHash,
   });
 
   userData.save();
